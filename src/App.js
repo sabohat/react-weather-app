1caa6f41fc64a55e1191264e70fcead8bfc1a874
sabohat <sabohat2802@gmail.com>
2021-08-27 17:56:35 +0500

    use dispatch in weather container

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './App.css';
-import {useSelector, useDispatch} from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { actionCreators } from './state/index';
 import WeatherContainer from './components/WeatherContainer';
 
 function App() {
 
-  const account = useSelector((state )=> state.account);
-  const dispatch = useDispatch();
-
-  const { depositMoney, withdrawMoney }= bindActionCreators(actionCreators, dispatch);
-
   return (
     <div className="App">
       <WeatherContainer/>
-      <h1>{ account }</h1>
-      <button onClick={()=>depositMoney(100)}>Deposit 100</button>
-      <button onClick={()=>withdrawMoney(100)}>Withdraw 100</button>
+     
    </div>
   );
 }
--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./../state/index";
 
-class WeatherContainer extends React.Component {
-  render() {
-    return (
+import React from "react";
+
+function WeatherContainer() {
+  const account = useSelector((state) => state.account);
+  const dispatch = useDispatch();
+
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+
+  return (
+    <>
       <div className="weather-container">HIEIHDIASD</div>
-    );
-  }
-
+      <h1>{account}</h1>
+      <button onClick={() => depositMoney(100)}>Deposit 100</button>
+      <button onClick={() => withdrawMoney(100)}>Withdraw 100</button>
+    </>
+  );
 }
 
 export default WeatherContainer;
