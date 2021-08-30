a635689ce350415e9b1244b1eb1691215c20ed7f
sabohat <sabohat2802@gmail.com>
2021-08-30 10:37:30 +0500

    add data container layout

--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -33,35 +33,53 @@ function WeatherContainer() {
           </div>
           <div className="weather__data">
             <div className="weather__degree">26*</div>
-            <div className="weather__details">
+            <div className="weather__data-details">
               <div className="weather__description">
-                <div className="weather__city">
-                  London
-                </div>
+                <div className="weather__city">London</div>
                 <div className="weather__description-text">
                   10:36 - Tuesday, 22 Oct '19
                 </div>
-                
               </div>
               <div className="weather__description">
-                <div className="weather__icon ">
-                  O
-                </div>
-                <div className="weather__description-text">
-                  Sunny
-                </div>
-                
+                <div className="weather__icon ">O</div>
+                <div className="weather__description-text">Sunny</div>
               </div>
-              
             </div>
           </div>
         </div>
 
-        <div className="weather-info"></div>
+        <div className="weather-info">
+          <form>
+          <input
+            className="weather-info__input"
+            placeholder="Another location"
+          ></input>
+          <button className="submit-button" type="submit">Search</button>
+</form>
+          <div className="weather__location">
+            <div className="cities">
+              <ul className="city-list">
+                <li className="city">Birmingham</li>
+                <li className="city">Birmingham</li>
+                <li className="city">Birmingham</li>
+                <li className="city">Birmingham</li>
+              </ul>
+            </div>
+          </div>
+          <div className="weather__details">
+            <div className="header"> <b>Weather Details</b> </div>
+            <div className="cities">
+              <ul className="data-list">
+                <li className="data"><span>Cloudy</span> <span>Data %</span></li>
+                <li className="data"><span>Humidity</span> <span>Data %</span></li>
+                <li className="data"><span>Wind</span> <span>Data km/h</span></li>
+                <li className="data"><span>Rain</span> <span>Data mm</span></li>
+                
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-      {/* <h1>{account}</h1>
-      <button onClick={() => depositMoney(100)}>Deposit 100</button>
-      <button onClick={() => withdrawMoney(100)}>Withdraw 100</button> */}
     </div>
   );
 }
