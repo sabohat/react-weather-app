a2045de386a75d15b7bb54eb16d8f0e7e8d08f46
sabohat <sabohat2802@gmail.com>
2021-09-02 23:35:12 +0500

    remove redux files

--- src/state/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import weatherReducer from "./weatherReducer";
-
-const reducers = combineReducers({
-  weather: weatherReducer,
-});
-
-export default reducers;
